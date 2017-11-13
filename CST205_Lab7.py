e6c07c85b5399df257dad7c426d0fc330c3037a5
grace289 <33207084+grace289@users.noreply.github.com>
2017-11-12 23:00:41 -0800

    Update CST205_Lab7.py

--- CST205_Lab7.py
+++ CST205_Lab7.py
@@ -8,7 +8,7 @@ CST205-40_FA17 Lab #7
 November 12th 2017
 '''
 
-#Thanksgiving Card
+#Removes green from image
 def chromaKey(pic, bgpic):
   pixels = getPixels(pic)
   bgPixels = getPixels(bgpic)
@@ -19,18 +19,40 @@ def chromaKey(pic, bgpic):
     if g > (b + r):
       setColor(p, getColor(getPixel(bgpic, getX(p), getY(p))))   
   
-  #add text
-  fontStyle = makeStyle(serif, italic + bold, 70)
-  addTextWithStyle(pic, 25, 400,'HAPPY THANKSGIVING!', fontStyle)
-  
+#calls chromaKey function
 def makeCard():
-  #Call Thanksgiving Card
   filename = pickAFile()
   pic = makePicture(filename)
   background = pickAFile()
   bgpic = makePicture(background)
   chromaKey(pic, bgpic)
-  repaint(pic)
+ # repaint(pic)
   writePictureTo(pic, "C:\\Users\\Grace\\Desktop\\CSUMB\\CST205\\Module 3\\Lab 7\\ThanksgivingCard.jpg")
 
+#Call makeCard() function
 makeCard()
+
+#Copying the image we created with the chromaKey function and using that as the background for the final card
+pic = makePicture("C:\\Users\\Grace\\Desktop\\CSUMB\\CST205\\Module 3\\Lab 7\\ThanksgivingCard.jpg")
+leavesPic = makePicture(pickAFile())
+card = makeEmptyPicture(852, 480)
+
+chromaKey(leavesPic, pic)  
+
+#pastes pic 
+for x in range(0, getWidth(pic)):
+  for y in range(0, getHeight(pic)):
+    color = getColor(getPixel(pic, x, y))
+    setColor(getPixel(card, x, y), color)
+#pastes leavesPic
+for x in range(0, getWidth(leavesPic)):
+  for y in range(0, getHeight(leavesPic)):
+    color = getColor(getPixel(leavesPic, x, y))
+    setColor(getPixel(card, x, y), color)
+  
+#adds text
+fontStyle = makeStyle(serif, italic + bold, 70)
+addTextWithStyle(card, 25, 450,'HAPPY THANKSGIVING!', fontStyle)
+
+repaint (card)
+writePictureTo(card, "C:\\Users\\Grace\\Desktop\\CSUMB\\CST205\\Module 3\\Lab 7\\ThanksgivingCardFinal.jpg")
