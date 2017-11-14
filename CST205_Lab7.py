2b0953eda643f5c8b60aa04c9cd06614810044ed
jgarcia17 <33381168+jgarcia17@users.noreply.github.com>
2017-11-13 19:23:47 -0800

    Update CST205_Lab7.py
    Warm-up coded added

--- CST205_Lab7.py
+++ CST205_Lab7.py
@@ -8,6 +8,29 @@ CST205-40_FA17 Lab #7
 November 12th 2017
 '''
 
+#Warm-up
+
+def drawSnowman():
+  filename = pickAFile()
+  pic = makePicture(filename)
+  colorWhite = makeColor(255, 255, 255)
+  colorBlack = makeColor(0, 0, 0)
+  for x in range(1, 200):
+    for y in range(0, 250):
+      px = getPixel(pic, x, y)
+      color = getColor(px)
+      addOvalFilled(pic, 500, 230, 200, 200, colorWhite)  #Top circle
+      addOvalFilled(pic, 500, 400, 200, 200, colorWhite)  #Middle Circle
+      addOvalFilled(pic, 500, 570, 200, 200, colorWhite)  #Bottom Circle
+      addOvalFilled(pic, 550, 270, 50, 50, colorBlack)    #Left eye Circle    
+      addOvalFilled(pic, 620, 270, 50, 50, colorBlack)    #Right eye Circle
+      addArcFilled(pic, 550, 300, 100, 100, 180, 180, colorBlack)  #Mouth
+      addOvalFilled(pic, 590, 450, 25, 25, colorBlack)    #Button top
+      addOvalFilled(pic, 590, 490, 25, 25, colorBlack)    #Button middle
+      addOvalFilled(pic, 590, 530, 25, 25, colorBlack)    #Button bottom
+  show(pic)
+  return(pic)
+
 #Removes green from image
 def chromaKey(pic, bgpic):
   pixels = getPixels(pic)
